a3d30b4078f05d571931a693faab5eefae4d39da
Verkhohliad <ihor.verkhohliad@gmail.com>
2019-02-22 20:08:39 +0200

    register service worker with logged event handlers

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,11 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register({
+  onSuccess(registration) {
+    window.console.log('Success: ', registration);
+  },
+  onUpdate(registration) {
+    window.console.log('Update: ', registration);
+  },
+});
