2ba183ebe448ac1db6df220b5b8dc9da9eb6d9be
Verkhohliad <ihor.verkhohliad@gmail.com>
2019-02-22 19:57:56 +0200

    add tslint, prettier, configs for them and fix tslint errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import 'react-app-polyfill/ie11';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
